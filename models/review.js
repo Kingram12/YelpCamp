e901c5a57dbded90eaafd8a45b4a1d5a36718e39
Kayla Ingram <kaylabsi@outlook.com>
2021-06-07 17:41:04 -0400

    post route for reviews

--- models/review.js
+++ models/review.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const reivewSchema = new Schema({
+const reviewSchema = new Schema({
     body: String,
     rating: Number
 });
