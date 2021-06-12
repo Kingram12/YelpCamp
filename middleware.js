42a8a79fcbe3b93fde5399144b92630822945086
Kayla Ingram <kaylabsi@outlook.com>
2021-06-12 19:00:25 -0400

    campground controller setup

--- middleware.js
+++ middleware.js
@@ -1,6 +1,7 @@
 const { campgroundSchema, reviewSchema } = require('./schemas.js');
 const ExpressError = require('./utils/ExpressError');
 const Campground = require('./models/campground');
+const review = require('./models/review');
 
 
 module.exports.isLoggedIn = (req, res, next) => {
@@ -27,6 +28,16 @@ module.exports.isAuthor = async(req, res, next) => {
     const {id} = req.params;
     const campground = await Campground.findById(id);
     if (!campground.author.equals(req.user._id)) {
+        req.flash('error', 'You do not have permission to do that!')
+        res.redirect(`/campgrounds/${id}`);
+    }
+    next();
+}
+
+module.exports.isReviewAuthor = async(req, res, next) => {
+    const {id, reviewId} = req.params;
+    const review = await Review.findById(reviewId);
+    if (!review.author.equals(req.user._id)) {
         req.flash('error', 'You do not have permission to do that!')
         res.redirect(`/campgrounds/${id}`);
     }
