dc232c0c5bf67257b077642715c2b7580715a195
Kayla Ingram <kaylabsi@outlook.com>
2021-06-12 20:24:44 -0400

    minor route restructure

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -18,12 +18,12 @@ module.exports.createCampground = async (req, res, next) => {
 }
 
 module.exports.showCampground = async (req, res,) => {
-    const campground = await (await Campground.findById(req.params.id).populate({
+    const campground = await Campground.findById(req.params.id).populate({
         path: 'reviews',
         populate: {
             path: 'author'
         }
-    }).populate('author'));
+    }).populate('author');
     if (!campground) {
         req.flash('error', 'Cannot find that campground!');
         return res.redirect('/campgrounds');
@@ -32,12 +32,12 @@ module.exports.showCampground = async (req, res,) => {
 }
 
 module.exports.renderEditForm = async (req, res) => {
-    const {id} = req.params;
-    const campground = await Campground.findById(id);
+    const { id } = req.params;
+    const campground = await Campground.findById(id)
     if (!campground) {
         req.flash('error', 'Cannot find that campground!');
         return res.redirect('/campgrounds');
-    }   
+    }
     res.render('campgrounds/edit', { campground });
 }
 
@@ -50,11 +50,6 @@ module.exports.updateCampground = async (req, res) => {
 
 module.exports.deleteCampground = async (req, res) => {
     const { id } = req.params;
-    const campground = await Campground.findById(id);
-    if (!campground.author.equals(req.user._id)) {
-        req.flash('error', 'You do not have permission to do that!')
-        res.redirect(`/campgrounds/${id}`);
-    }
     await Campground.findByIdAndDelete(id);
     req.flash('success', 'Successfully deleted campground')
     res.redirect('/campgrounds');
--- models/campground.js
+++ models/campground.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
-const Review = require('./review');
+const Review = require('./review')
 const Schema = mongoose.Schema;
 
-const CampgroundSchema = new Schema ({
+const CampgroundSchema = new Schema({
     title: String,
     image: String,
     price: Number,
@@ -14,14 +14,14 @@ const CampgroundSchema = new Schema ({
     },
     reviews: [
         {
-        type: Schema.Types.ObjectId,
-        ref: 'Review'
-    }
-]
-})
+            type: Schema.Types.ObjectId,
+            ref: 'Review'
+        }
+    ]
+});
 
-CampgroundSchema.post('findOneAndDelete', async function(doc) {
-    if(doc){
+CampgroundSchema.post('findOneAndDelete', async function (doc) {
+    if (doc) {
         await Review.deleteMany({
             _id: {
                 $in: doc.reviews
@@ -29,6 +29,5 @@ CampgroundSchema.post('findOneAndDelete', async function(doc) {
         })
     }
 })
-
 
 module.exports = mongoose.model('Campground', CampgroundSchema);
