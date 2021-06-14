e7e5156f5ae359f8f32ba9b1e4d72ccd86db8bb1
Kayla Ingram <kaylabsi@outlook.com>
2021-06-13 21:04:33 -0400

    file fix

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,5 @@
 const User = require('../models/user');
 
-
 module.exports.renderRegister = (req, res) => {
     res.render('users/register');
 }
@@ -21,7 +20,7 @@ module.exports.register = async (req, res, next) => {
     }
 }
 
-module.exports.renderLogin =  (req, res) => {
+module.exports.renderLogin = (req, res) => {
     res.render('users/login');
 }
 
@@ -34,6 +33,7 @@ module.exports.login = (req, res) => {
 
 module.exports.logout = (req, res) => {
     req.logout();
+    // req.session.destroy();
     req.flash('success', "Goodbye!");
     res.redirect('/campgrounds');
 }
